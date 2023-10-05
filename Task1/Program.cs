655faa44b0971a4b2cb9522d058bac4e828449e6
Yasser <myasser@mail.ru>
2023-10-05 18:18:15 +0300

    added the answers for taskes

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16 
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите Первый Число");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ввидите Второй Число");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        int c = 1;
+        for (int i = 1; i<=b; i++)
+        {
+            c = c * a;
+        }    
+        
+            Console.Write($"-> {c}");
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 29: Напишите программу, которая принимает на вход длину массива и в последующих строках элементы массива.
+// 5
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 3
+// 6, 1, 33 -> [6, 1, 33] 
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Вводите Число Элементов Массива");
+        int n = int.Parse(Console.ReadLine());
+        int[] array = new int[n];
+        for (int i = 0; i < array.Length; i++)
+            array[i] = new Random().Next(0, 999);
+
+        Console.WriteLine($"[{string.Join(", ", array)}]");
+    }
+}
